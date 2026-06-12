450b3a0c6b048dec5085b6472e1f351530834736
reddytk2006-tech <reddytk2006@gmail.com>
2026-06-12 23:32:11 +0530

    student_management.py

--- student_management.py
+++ student_management.py
@@ -1,1 +1,32 @@
+students = {}
 
+while True:
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        roll = input("Enter Roll Number: ")
+        name = input("Enter Name: ")
+        students[roll] = name
+        print("Student Added Successfully!")
+
+    elif choice == "2":
+        for roll, name in students.items():
+            print(roll, "-", name)
+
+    elif choice == "3":
+        roll = input("Enter Roll Number: ")
+        if roll in students:
+            print("Student Name:", students[roll])
+        else:
+            print("Student Not Found")
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid Choice")
